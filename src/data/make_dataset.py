34abf7c8b43d76cf51602fb7f3857f8711441f1e
cnrprod-team-18733 <1635726043@codenrock.com>
2022-10-10 04:54:47 +0300

    1

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,31 @@
 # -*- coding: utf-8 -*-
+import os
+import requests
+import pandas as pd
+import pickle
+from tqdm.auto import tqdm
+import urllib.request
+import numpy as np
+import seaborn as sns
+from time import sleep
 import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+@click.group()
+def cli():
+    pass
 
+@cli.command(name='getUniqFilms')
+def get_uniq_films():
+    kp = pd.read_csv('data/external/kinopoisk.csv',encoding='windows-1251')
+    kp['NAME'] = kp['NAME'].replace(' +', ' ', regex=True)
+
+    imdb = pd.read_csv('data/external/imdb_db.csv')
+
+    films_set = list(set(list(kp['NAME'].dropna()) + list(imdb['NAME'].dropna())))
+
+    with open('data/raw/films_set.pkl', 'wb') as handle:
+        pickle.dump(films_set, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+    cli()
