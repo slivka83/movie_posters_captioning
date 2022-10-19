c1d63bca054642057ad4ee352e09f8392848be8b
BLAIN <Jenya926@gmail.com>
2022-10-19 17:16:29 +0300

    отчет2010

--- app/dashboard/dashboard/settings.py
+++ app/dashboard/dashboard/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from data.config import DJANGO_KEY
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kf-k9gb488bn6wqu1gvu4l63zo857k9m))ale9_*e-9=!9*k=r'
+SECRET_KEY = DJANGO_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- app/data/config.py
+++ app/data/config.py
@@ -12,4 +12,5 @@ DB_USER = env.str("DB_USER")
 DB_PASS = env.str("DB_PASS")
 DB_NAME = env.str("DB_NAME")
 DB_HOST = env.str("DB_HOST")
+DJANGO_KEY = env.str("DJANGO_KEY")
 
